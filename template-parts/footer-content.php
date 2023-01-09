64e005b4d06d3e4c7038b3ad763f9488f3a68c5c
vlasiuk44 <55242330+vlasiuk44@users.noreply.github.com>
2023-01-10 01:57:26 +0800

    Add files via upload

---
+++ template-parts/footer-content.php
@@ -0,0 +1,5 @@
+<footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-ac95">
+  <div class="u-clearfix u-sheet u-sheet-1">
+    <p class="u-small-text u-text u-text-variant u-text-1">Пример текста. Кликните, чтобы выбрать элемент "Текст".</p>
+  </div>
+</footer>
